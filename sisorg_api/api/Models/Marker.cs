e8c784582965e8af0ce2d3f83a4c7f9242b199c9
Lucas Beckford <103231186+lucasbeck1@users.noreply.github.com>
2023-08-14 13:56:57 -0300

    upload api

---
+++ sisorg_api/api/Models/Marker.cs
@@ -0,0 +1,18 @@
+﻿namespace api.Models
+{
+    public class Marker
+    {
+        public int ID { get; set; }
+        public int Count { get; set; }
+        public DateTime Timestamp { get; set; }
+        public List<Country> Rows { get; set; }
+        public Marker() { }
+        public Marker(int id, int count, DateTime timeStamp, List<Country> countries)
+        {
+            this.ID = id;
+            this.Count = count;
+            this.Timestamp = timeStamp;
+            this.Rows = countries;
+        }
+    }
+}
